0fae40bc4fbca1c4f387ed671c35ac6899b92b65
Sohit Kumar <samratsohit@gmail.com>
2025-08-22 02:05:50 +0530

    day - 02 add

---
+++ Day-02/app.js
@@ -0,0 +1,53 @@
+// Qs. 1: Write a javaScript program to get the first n elements of an array.
+// [n can be any positive number].
+// For example: for array [7, 9, 0, -2] and n = 3
+// Print, [7, 9, 0]
+
+// let arr = [7, 9, 0,-2];
+// let n = 3;
+// let ans = arr.slice(0, n);
+// console.log(ans);
+
+
+// Qs. 2:Write a javaScript program to get the first n elements of an array.
+// [n can be any positive number].
+// For example: for array [7, 9, 0, -2] and n = 3
+// Print, [9, 0, -2]
+
+// let arr = [7, 9, 0, -2];
+// let n =3;
+// let ans = arr.slice(arr.length - n);
+// console.log(ans);
+
+// Qs. 3: Write a JavsScript to check whether a string is blank or not.
+// let str = prompt("Please enter a string");
+// if (str.length ===0){
+//     console.log("String is blank");
+// }else{
+//     console.log("String is not blank");
+// }
+
+// Qs. 4: Write a javsScript program to test whether the character at the given (character) index is lower case.
+// let str = "ApNaCoLlEgE";
+// let idx = 5;
+// if (str[idx] == str[idx].toLowerCase()){
+//     console.log("character is lowercase");
+// }else{
+//     console.log("character is not lowercase");
+// }
+
+// Qs. 5: Write a JavaScript program to strip leading and trailing spaces from a string.
+// let str = prompt("Please enter a string");
+// console.log(`Original string = ${str}`);
+// console.log(`String without spaces = ${str.trim()}`);
+
+
+// Qs. 6: Write a JavaScript program to check if an element exists in an array or not.
+// let arr = ["hello", 'a', 23, 64, 99, -6];
+// let item = 64;
+// if (arr.indexOf(item) != -1){
+//     console.log("Element exists in array");
+// }else{
+//     console.log("Element doesn't exist in array");
+    
+// }
