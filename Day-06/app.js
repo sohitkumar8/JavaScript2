f5f2328fdb6e2b882209ee9f1bd685b81a91c9e2
Sohit Kumar <samratsohit@gmail.com>
2025-09-10 01:03:37 +0530

    Day-07 add

---
+++ Day-06/app.js
@@ -0,0 +1,44 @@
+// let arr = [1,2,3,4,5,6,2,3];
+// let num = 2;
+
+
+// for(let i=0; i < arr.length; i++){
+//     if(num == arr[i]){
+//         arr.splice(i, 1);
+//     }
+// }
+// console.log(arr);
+
+
+// let number = 287152;
+// let count = 0;
+
+// let copy = number;
+// while(copy > 0){
+//     count++;
+//     copy = Math.floor(copy/10);
+// }
+// console.log(count);
+
+
+
+// let number = 287152;
+// let sum = 0;
+
+// let copy = number;
+// while(copy > 0){
+//     digit = copy%10;
+//     sum+=digit;
+//     copy = Math.floor(copy/10);
+// }
+// console.log(sum);
+
+
+// let n = 5;
+// let factorial = 1;
+// for(let i = 1; i<=n; i++){
+//     factorial*= i
+// }
+// console.log(factorial);
+
+let arr = [2,3,10, 5,7,4,8,9];
---
+++ Day-07/app.js
@@ -0,0 +1,113 @@
+//  JS Objects
+
+// const student = {
+//     name: "Ravi",
+//     age: 21,
+//     marks: 93.5
+// };
+
+// console.log(student);
+
+
+// Thread / Twitter Post
+// Create an object literal for the properties of thread / thread twitter post which includes -
+// usename, content, likes, reposts, tags.
+
+// const post ={
+//     userName: "@samratsohit",
+//     content: "This is my #firstPost",
+//     likes: 150,
+//     repost: 5,
+//     tags: ["@apnacollege", "#delta"]
+// };
+
+// console.log(post.userName);
+// console.log(post["content"]);
+// console.log(post);
+
+
+
+// Add / Update Value
+// Change the city to "Mumbai"
+// Add a new property, gender: "Female"
+// Change the marks to "A"
+
+// const student = {
+//     name: "Sohit",
+//     age: 20,
+//     marks: 92.3,
+//     city: "Delhi"
+// };
+
+// console.log("before change");
+
+// delete student.marks;
+// delete student.city;
+
+// console.log(student);
+
+// console.log("After change");
+
+// student.city = "Mumbai";
+// student.gender = "male"
+// student.marks = "A"
+// console.log(student);
+
+
+
+// Object of Object
+// Storing information of multiple students.
+
+// const classInfo = {
+//     aman: {
+//         grade: "A+",
+//         city: "Delhi"
+//     },
+//     soni: {
+//         grade: "A",
+//         city: "Pune"
+//     },
+//     karan: {
+//         grade: "O",
+//         city: "Mumbai"
+//     }
+// };
+
+// classInfo.soni.city = "Gurgao";
+
+// console.log(classInfo);
+
+
+
+// Array of Objects
+// Storing information of multiple students
+// const classInfo = [
+//     {
+//         name: "ravi",
+//         grade: "A+",
+//         city: "Delhi"
+//     },
+//     {
+//         name: "anjali",
+//         grade: "A+",
+//         city: "Pune"
+//     },
+//     {
+//         name: "rohan",
+//         grade: "O",
+//         city: "Mumbai"
+//     }
+// ];
+
+// classInfo[0].city = "Patna"; // change city 
+// console.log(classInfo[0].city);
+// console.log(classInfo[1].name);
+// console.log(classInfo[2].grade);
+
+// console.log(classInfo);
+
+
+
+//      Math Object
+// properties :- Math.PI, Math.E
+// Methods :- Math.abs(n), Math.pow(a,b), Math.floor(n), Math.ceil(n), Math.random()
